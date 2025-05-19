3089136ce43a231c29607fbc8d5f98a025203670
Thong Thien <thongthien2004@gmail.com>
2025-05-19 16:40:39 +0700

    sua import db

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 class Config:
     SECRET_KEY = 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/duan_db_py'
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/duan_db_py'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://thongthien2004:YourPassword@thongthien2004.mysql.pythonanywhere-services.com/thongthien2004$duan_db_py'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
